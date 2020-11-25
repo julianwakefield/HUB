79c6fbfd227df67d69fbc303500864bf51b59dc9
Tobias Sangster <tobias@greysealcoffee.com>
2020-11-25 10:28:35 +0000

    Cloudinary looking good

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,5 @@ class Game < ApplicationRecord
   # has_many :user_chats, :through => :chat
   has_many :comments, :through => :posts
   # has_many :messages, :through => :chat
+  has_one_attached :photo
 end
