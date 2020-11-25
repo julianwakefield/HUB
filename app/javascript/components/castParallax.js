4fd3ae102018d3defc1253371925ac0e36a69197
Tobias Sangster <tobias@greysealcoffee.com>
2020-11-25 15:14:44 +0000

    homepage done

---
+++ app/javascript/components/castParallax.js
@@ -0,0 +1,23 @@
+
+function castParallax() {
+
+	var opThresh = 350;
+	var opFactor = 750;
+
+	window.addEventListener("scroll", function(event){
+
+		var top = this.pageYOffset;
+
+		var layers = document.getElementsByClassName("parallax");
+		var layer, speed, yPos;
+		for (var i = 1; i < layers.length; i++) {
+			layer = layers[i];
+			speed = layer.getAttribute('data-speed');
+			var yPos = -(top * speed / 100);
+			layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+
+		}
+	});
+}
+
+document.body.onload = castParallax();
