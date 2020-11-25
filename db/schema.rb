534c08172ee98f5204f89eda56989f1b545ac999
Julian Wakefield <julianwakefield@outlook.com>
2020-11-25 12:14:08 +0000

    env

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_111751) do
+ActiveRecord::Schema.define(version: 2020_11_24_161858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 2020_11_25_111751) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "content"
-    t.string "image"
     t.index ["game_id"], name: "index_posts_on_game_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
@@ -89,7 +88,6 @@ ActiveRecord::Schema.define(version: 2020_11_25_111751) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "nickname"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
