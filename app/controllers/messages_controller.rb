e49f3c13d1415dd6dc1891c4d32316c5fbf389c5
silvia pintus <silviapintus.93@gmail.com>
2020-11-26 12:27:29 +0000

    chat almost ready

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,20 @@
 class MessagesController < ApplicationController
+
+  def create
+    @chat = Chat.find(params[:chat_id])
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.user = current_user
+    if @message.save
+      redirect_to game_path(@chat.game, anchor: "message-#{@message.id}")
+    else
+      render "chats/show"
+    end
+  end
+
+  private 
+  def message_params
+    params.require(:message).permit(:message)
+  end
+
 end
