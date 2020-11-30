c4bc3670828387da9585a0c18b03db3435fbe13c
Julian Wakefield <julianwakefield@outlook.com>
2020-11-30 17:55:46 +0000

    scraping news articles

---
+++ app/services/news_scraper.rb
@@ -0,0 +1,16 @@
+# require 'byebug'
+# require 'nokogiri'
+# require 'open-uri'
+
+# class NewsScraper
+#     def initialize
+#         html2 = open("https://www.pcgamer.com/uk/news/")
+#         articles = Nokogiri::HTML(html2)
+#         article_titles = doc.css(".article-name")  
+#         byebug
+#     end
+
+
+# end
+
+# article_titles.first.children.text
