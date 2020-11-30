2a9508b8ae41f12e8b3c9ed5a54cb3a4a606ae7a
Tobias Sangster <tobias@greysealcoffee.com>
2020-11-30 14:15:52 +0000

    updates

---
+++ app/javascript/components/carousel.js
@@ -0,0 +1,17 @@
+import Siema from 'siema'; 
+const carousel = () => {
+    const mySiema = new Siema({
+        selector: '.games',
+        perPage: {
+          768: 2,
+          1024: 3,
+        },
+      });
+      const prev = document.querySelector('.prev');
+      const next = document.querySelector('.next');
+      
+      prev.addEventListener('click', () => mySiema.prev());
+      next.addEventListener('click', () => mySiema.next());
+};
+    
+export {carousel};
