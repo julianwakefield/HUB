8f4c9bad3fd855e4ef4a3793a43012edd3ae32b1
Tobias Sangster <tobias@greysealcoffee.com>
2020-12-01 16:22:20 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_103725) do
+ActiveRecord::Schema.define(version: 2020_12_01_152058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_103725) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint "game_id", null: false
+    t.bigint "game_id"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
