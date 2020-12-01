b7625cc0e110c32cb114a76ce126a7f98a761297
Julian Wakefield <julianwakefield@outlook.com>
2020-12-01 10:04:56 +0000

    news scraper

--- app/services/news_scraper.rb
+++ app/services/news_scraper.rb
@@ -1,16 +1,16 @@
-# require 'byebug'
-# require 'nokogiri'
-# require 'open-uri'
+require 'byebug'
+require 'nokogiri'
+require 'open-uri'
 
-# class NewsScraper
-#     def initialize
-#         html2 = open("https://www.pcgamer.com/uk/news/")
-#         articles = Nokogiri::HTML(html2)
-#         article_titles = doc.css(".article-name")  
-#         byebug
-#     end
+class NewsScraper
+    def initialize
+        html2 = open("https://www.pcgamer.com/uk/news/")
+        articles = Nokogiri::HTML(html2)
+        article_titles = doc.css(".article-name")  
+        byebug
+    end
 
 
-# end
+end
 
 # article_titles.first.children.text
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ game_names.first(25).each do |game|
     Game.create(name: game.text)
 end
 
-# html2 = open("https://www.pcgamer.com/uk/news/")
-# news = Nokogiri::HTML(html2)
-# titles = news.css(".article-name")  
-# article_titles.children.first(5).each do |article|
-#     Article.create(title: article.text)
-# end
+html2 = open("https://www.pcgamer.com/uk/news/")
+news = Nokogiri::HTML(html2)
+titles = news.css(".article-name")  
+article_titles.children.first(5).each do |article|
+    Article.create(title: article.text)
+end
