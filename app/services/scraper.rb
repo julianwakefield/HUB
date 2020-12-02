04528c2031adbc15bf70c557c1123e08380212af
Julian Wakefield <julianwakefield@outlook.com>
2020-12-02 12:47:49 +0000

    games scraped and populated from EA

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -4,11 +4,37 @@ require 'open-uri'
 
 class Scraper
     def initialize
-        html = open("https://store.steampowered.com/search/?term=")
+        html = open("https://www.ea.com/games/library/action", 'User-Agent' => 'firefox')
         doc = Nokogiri::HTML(html)
-        game_names = doc.css(".title")  
+        gamelist = doc.css("ea-game-box")
+       
+            gamelist.first.attr('background-image') gamelist.first.attr('title')
         byebug
+        # end
     end
 
 
 end
+
+# gamelist.css("tr").first.css(".title > h3").text
+
+
+# require 'byebug'
+# require 'nokogiri'
+# require 'open-uri'
+
+# class Scraper
+#     def initialize
+#         html = open("https://www.metacritic.com/browse/games/score/metascore/all/pc/filtered", 'User-Agent' => 'firefox')
+#         doc = Nokogiri::HTML(html)
+#         gamelist = doc.css(".clamp-list").css("tr").first(25).each do |game|
+#             #   p game.css(".title > h3").text
+#             game.css(".clamp-image-wrap").css("img").attr('src').value
+#         byebug
+#         end
+#     end
+
+
+# end
+
+# gamelist.css("tr").first.css(".title > h3").text
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,69 @@
 require 'byebug'
 require 'nokogiri'
 require 'open-uri'
+Article.destroy_all
+Game.destroy_all
 
 
-html = open("https://store.steampowered.com/search/?term=")
+html = open("https://www.ea.com/games/library/action", 'User-Agent' => 'firefox')
 doc = Nokogiri::HTML(html)
-game_names = doc.css(".title")  
-game_names.first(25).each do |game|
-    Game.create(name: game.text)
+gamelist = doc.css("ea-game-box").first(25).each do |game| 
+    begin
+        title = game.attr('title')
+        image = game.attr('background-image')
+        file = URI.open(image)
+        game1 = Game.new
+        game1.name = title
+        game1.photo.attach(io: file, filename: 'image.png', content_type: 'image/png')
+        # byebug
+        game1.save
+    rescue
+    end
 end
-
-
 
 html2 = open("https://www.pcgamer.com/uk/news/")
 articles = Nokogiri::HTML(html2)
 articles.search(".article-lead-image-wrap").first(15).each do |url|
     Article.create(title: url.parent.parent.parent.attributes["aria-label"].value, image_url: url.attributes["data-original"].value)
 end
+
+
+# html = open("https://www.ea.com/games/library/action", 'User-Agent' => 'firefox')
+#         doc = Nokogiri::HTML(html)
+#         gamelist = doc.css("ea-game-box")
+       
+#             gamelist.first.attr('background-image') gamelist.first.attr('title')
+#         byebug
+#         # end
+
+
+
+
+# require 'byebug'
+# require 'nokogiri'
+# require 'open-uri'
+# Article.destroy_all
+# Game.destroy_all
+
+
+# html = open("https://www.metacritic.com/browse/games/score/metascore/all/pc/filtered", 'User-Agent' => 'firefox')
+# doc = Nokogiri::HTML(html)
+# gamelist = doc.css(".clamp-list").css("tr").first(25).each do |game| 
+#     begin
+#         title = game.css(".title > h3").text
+#         image = game.css(".clamp-image-wrap").css("img").attr('src').value
+#         file = URI.open(image)
+#         game1 = Game.new
+#         game1.name = title
+#         game1.photo.attach(io: file, filename: 'image.png', content_type: 'image/png')
+#         game1.save
+#     rescue
+#     end
+# end
+
+# html2 = open("https://www.pcgamer.com/uk/news/")
+# articles = Nokogiri::HTML(html2)
+# articles.search(".article-lead-image-wrap").first(15).each do |url|
+#     Article.create(title: url.parent.parent.parent.attributes["aria-label"].value, image_url: url.attributes["data-original"].value)
+# end
+
